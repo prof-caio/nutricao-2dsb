b175031f588f2628b916f2177d9ee4f07dd2ed7c
prof-caio <126119206+prof-caio@users.noreply.github.com>
2023-09-04 11:45:52 -0300

    inicio até alteração do titulo

---
+++ js/principal.js
@@ -0,0 +1,9 @@
+//-------------------------------------
+//daqui pra baixo começa o js do site
+
+//busca no site pelo titulo e armazena ele dentro da variável titulo
+let titulo = document.querySelector(".titulo");
+//mostra no devtools o conteudo de texto da variavel titulo para fins de teste
+console.log(titulo.textContent);
+//altera o conteudo da variável titulo para o texto desejado
+titulo.textContent = "Fica Grande Nutrição";
